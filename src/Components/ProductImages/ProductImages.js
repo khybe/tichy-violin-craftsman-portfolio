cde3f48ee6894047b8f21d8559cd8b02e7f4b9be
Khaibar Saadat <khybersaadat93@gmail.com>
2024-03-21 18:42:42 +0100

    Add dynamic class support in ProductImages component

--- src/Components/ProductImages/ProductImages.js
+++ src/Components/ProductImages/ProductImages.js
@@ -1,11 +1,11 @@
 import React from "react";
 import "./ProductImages.css";
 
-const ProductImages = ({ images }) => {
+const ProductImages = ({ images, className }) => {
   return (
     <div className="product-images-container">
       {images.map((image, index) => (
-        <div className="product-image" key={index}>
+        <div className={`product-image ${className}`} key={index}>
           <img src={image} alt={`Product ${index}`} />
         </div>
       ))}
