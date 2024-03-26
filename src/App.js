e2c5027944630a502f06c1f61a2a1788cd81f4ef
Khaibar Saadat <khybersaadat93@gmail.com>
2024-03-26 21:18:57 +0100

    Optimization: improved component loading with lazy loading and suspense in App.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
-import { useState } from "react";
+import { lazy, Suspense, useState } from "react";
 import { Routes, Route } from "react-router-dom";
 
 import Header from "./Components/Header/Header";
 import HomePage from "./Pages/HomePage";
-import ViolinPage from "./Pages/ViolinPage";
-import ViolaPage from "./Pages/ViolaPage";
-import CelloPage from "./Pages/CelloPage";
-import GambaPage from "./Pages/GambaPage";
 import Footer from "./Components/Footer/Footer";
+
+const ViolinPage = lazy(() => import("./Pages/ViolinPage"));
+const ViolaPage = lazy(() => import("./Pages/ViolaPage"));
+const CelloPage = lazy(() => import("./Pages/CelloPage"));
+const GambaPage = lazy(() => import("./Pages/GambaPage"));
 
 function App() {
   // State to manage selected language
@@ -27,29 +28,37 @@ function App() {
       />
       <main>
         {/* Routing for different pages */}
-        <Routes>
-          <Route
-            path="/"
-            element={<HomePage language={selectedLanguage} />}
-            exact={true}
-          />
-          <Route
-            path="/housle"
-            element={<ViolinPage language={selectedLanguage} />}
-          />
-          <Route
-            path="/viola"
-            element={<ViolaPage language={selectedLanguage} />}
-          />
-          <Route
-            path="/violoncello"
-            element={<CelloPage language={selectedLanguage} />}
-          />
-          <Route
-            path="/violadagamba"
-            element={<GambaPage language={selectedLanguage} />}
-          />
-        </Routes>
+        <Suspense
+          fallback={
+            <div style={{ textAlign: "center", marginTop: "50px" }}>
+              Loading...
+            </div>
+          }
+        >
+          <Routes>
+            <Route
+              path="/"
+              element={<HomePage language={selectedLanguage} />}
+              exact={true}
+            />
+            <Route
+              path="/housle"
+              element={<ViolinPage language={selectedLanguage} />}
+            />
+            <Route
+              path="/viola"
+              element={<ViolaPage language={selectedLanguage} />}
+            />
+            <Route
+              path="/violoncello"
+              element={<CelloPage language={selectedLanguage} />}
+            />
+            <Route
+              path="/violadagamba"
+              element={<GambaPage language={selectedLanguage} />}
+            />
+          </Routes>
+        </Suspense>
       </main>
       {/* Footer component */}
       <Footer language={selectedLanguage} />
